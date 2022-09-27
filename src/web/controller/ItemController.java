47aa76d4c4705d18357519c756b4dd8541bb687e
KOSA <KOSA@DESKTOP-6FO5ULH>
2022-09-27 14:06:47 +0900

    컨트롤러 수정

--- src/web/controller/ItemController.java
+++ src/web/controller/ItemController.java
@@ -7,9 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ItemController
- */
+@WebServlet("*.item")
 public class ItemController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
